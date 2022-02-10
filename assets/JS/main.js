cde9e048d7307b4591626b9302ccfe608fdc498a
yatzc <wattier.yann-81@hottmail.fr>
2022-02-10 20:09:46 +0100

    main search finish

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -3,6 +3,7 @@ import * as Btn from './btn.js';
 // import * as Dishe from './dishes.js';
 import { unique_ingredients, order_unique_appliance, order_unique_ustensils } from "./data.js";
 import { recipes } from "./recipes.js";
+import * as seach from "./searchMain.js";
 
 // #region ============ affichage contenu des trois boutons
 // affichage des ingredient
@@ -24,6 +25,8 @@ function ustensilsTemplate(item) { return `<li>${item}</li>`; }
 // #endregion ============ affichage contenu des trois boutons
 
 // #region ============ affichage des plats
+
+// display des ingrédients
 function ingredientsDisplay(ingredients) {
     return `
     <ul role="menu" class="tag-list">
@@ -39,11 +42,12 @@ function ingredientsDisplay(ingredients) {
     `;
 }
 
+// display des articles
 function dishesdisplay(dishe) {
     return`
     <article>
         <div class="container-dishes">
-            <div class="container-dishes-img"><img src="/assets/images/${dishe.image}.jpg" alt="/images/${dishe.image}.jpg"></div>
+            <div class="container-dishes-img"><img src="./assets/images/${dishe.image}.jpg" alt="/images/${dishe.image}.jpg"></div>
             <div class="dishes-name">${dishe.name}</div>
             <div class="dishes-time"><img src="./assets/images/watch.svg" alt="watch time" />${dishe.time} min</div>
             <div class="dishes-ingredients">${dishe.ingredients ? ingredientsDisplay(dishe.ingredients) : ""}</div>
@@ -56,24 +60,33 @@ function dishesdisplay(dishe) {
 document.getElementById("articles").innerHTML = `${recipes.map(dishesdisplay).join("")}`;
 // #endregion ============ affichage des plats
 
-// #region ============ SEARCH
+// #region ============ SEARCH MAIN
 const searchinput = document.getElementById("main_search");
 
 searchinput.addEventListener('keyup', function(){
     // recupère la valeur du input
-    const input = searchinput.value;
+    const inputValue = searchinput.value;
     // compare le input à personnes
-    const result = recipes.filter(item => item.name.toLowerCase().includes(input.toLowerCase()));
+    const result = recipes.filter(item => item.name.toLowerCase().includes(inputValue.toLowerCase()));
+
     // Met chaque resultat dans un div
     let suggestion = '';
-    if(input != "") {
-        result.forEach(resultItem => suggestion +=`
-            ${dishesdisplay(resultItem)}
-        `);
+    if(inputValue != "") {
+        result.forEach(resultItem => suggestion +=`${dishesdisplay(resultItem)}`);
     } else {
-        dishesdisplay();
+        suggestion = `${recipes.map(dishesdisplay).join("")}`;
     }
     // Affichage de chaque resultat
     document.getElementById("articles").innerHTML = suggestion;
 });
-// #endregion ============ SEARCH
+
+// #endregion ============ SEARCH MAIN
+
+
+
+
+
+
+
+
+
